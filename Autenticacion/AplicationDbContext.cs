671de5203c3784de1e4fbfd626a1b552b7c05d75
oxipusio <oxipusioxx12@gmail.com>
2022-11-10 17:29:50 -0500

    Setup configuration and structure

---
+++ Autenticacion/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using Autenticacion.Models;
+
+namespace Autenticacion
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Mapeo de las tablas
+        //public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }   
+}
---
+++ CreateTale/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using CreateTale.Models;
+
+namespace CreateTale
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Mapeo de las tablas
+        //public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }   
+}
