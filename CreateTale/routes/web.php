edf185fbae0631dbcb9dd6f283fa710b1ba2f1ad
oxipusio <oxipusioxx12@gmail.com>
2022-12-17 04:10:24 -0500

    .

--- CreateTale/routes/web.php
+++ CreateTale/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\TalesController;
 
 Route::get('/', [TalesController::class,'borrar']);
 Auth::routes();
