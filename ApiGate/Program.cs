e8f814db30857ec46beb85535cafb2045e0cc3b5
oxipusio <oxipusioxx12@gmail.com>
2022-12-18 21:55:44 -0500

    Reworked apigateway

--- ApiGate/Program.cs
+++ ApiGate/Program.cs
@@ -1,24 +1,20 @@
-using ApiGateway;
 using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-
-namespace ApiGateway
+namespace ApiGate
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
-
-
             var builder = WebApplication.CreateBuilder(args);
             var routes = "Routes";
             builder.Configuration.AddOcelotWithSwaggerSupport(options =>
             {
                 options.Folder = routes;
             });
+            // Add services to the container.
 
             builder.Services.AddOcelot();
             builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
@@ -26,19 +22,22 @@ namespace ApiGateway
                 .AddEnvironmentVariables();
 
             builder.Services.AddControllers();
-
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
             var app = builder.Build();
 
-            app.UseSwagger();
-            app.UseSwaggerUI();
-
-            //app.UseHttpsRedirection();
+            // Configure the HTTP request pipeline.
+            //if (app.Environment.IsDevelopment())
+            //{
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            //}
 
             app.UseAuthorization();
+
 
             app.MapControllers();
             app.UseSwaggerForOcelotUI(options =>
