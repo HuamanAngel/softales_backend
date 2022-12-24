426d1fed28ae9e8c2a2495fe6e1e38e1432a5e63
Frank Pomiano <frank.pomiano@unmsm.edu.pe>
2022-12-24 00:45:05 -0500

    test collection-tales authControllerTest 2

---
+++ VerHistoria/tests/Unit/CollectionTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Collection;
+use Tests\TestCase;
+use App\Http\Controllers\CollectionController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+
+     
+
+
+    /** @test */ 
+    public function IndexCollectionTest()
+    {
+        $coll = Collection::all();
+        $CollController = new CollectionController();
+        $response = $CollController->index();
+        //$this->assertTrue(true);
+        $responseContent = $response->getOriginalContent()[0];
+
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals((string) $coll,(string) $responseContent);
+    }
+
+
+    public function storeCollectionTest()
+    {
+        $request = new Request([
+            "algo"=>"awdwa"
+        ]);
+        $CollController = new CollectionController();
+        $response = $CollController->store($request);
+        $responseContent = $response->getOriginalContent();
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals("Insertar token Bearer", $responseContent["message"]);
+    }
+
+    public function storeCollectionTest1()
+    {
+        $request = new Request([
+            "col_titl"=>"Cuentos de la cripta Temp 2",
+            "col_desc"=>"Todos los cuentos de la cripta temporada 2",
+            "col_cate"=>"Terror",
+            'user_id' => 1
+        ]);
+        $CollController = new CollectionController();
+        $response = $CollController->store($request);
+        $responseContent = $response->getOriginalContent();
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals("Creado correctamente", $responseContent["message"]);
+    }
+}
